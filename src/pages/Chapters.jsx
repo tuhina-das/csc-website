513307da4cddebb62571eb6bfed11c78553dd0ce
Tuhina Das <tuhinakdas@gmail.com>
2024-03-13 02:03:01 -0500

    began laying out Chapters page HTML/CSS

--- src/pages/Chapters.jsx
+++ src/pages/Chapters.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import '../styles/Chapters.css';
+import HeroImg from '../assets/Chapters/startChapterBg.webp';
 
 function Chapters() {
 
     return (
-    <div>
-       Chapters
-    </div>
+        <>
+            <div className="chapter-hero">
+                <img className="chapter-hero-img" src={HeroImg}></img>
+            </div>
+            <div className="why-box">
+                <div className="why-header">
+                    <h1>Why you should start one...</h1>
+                </div>
+                <div className="why-information">
+                    
+                </div>
+            </div>
+        </>
     )
 };
 
